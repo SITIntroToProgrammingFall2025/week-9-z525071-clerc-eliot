679586dbaf27055ccc1ed0fc29c52bf4a3b040a5
z525071-clerc-eliot <z525071@shibaura-it.ac.jp>
2025-12-03 11:51:27 +0900

    Update hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+
+#define MATRIX_SIZE 3
+
+int main() {
+    int matrix[MATRIX_SIZE][MATRIX_SIZE];
+
+    for(int i = 0; i < MATRIX_SIZE; i++) {
+        for(int j = 0; j < MATRIX_SIZE; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("You entered\n");
+
+    for(int i = 0; i < MATRIX_SIZE; i++) {
+        for(int j = 0; j < MATRIX_SIZE; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    printf("Output\n");
+
+    for(int i = 0; i < MATRIX_SIZE; i++) {
+        for(int j = MATRIX_SIZE - 1; j >= 0; j--) {
+            printf("%d ", matrix[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
 
