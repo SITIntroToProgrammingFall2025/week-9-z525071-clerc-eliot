9a8c9db6f314c58fcec42c99a2b12c969d9bb6d4
z525071-clerc-eliot <z525071@shibaura-it.ac.jp>
2025-12-03 15:42:36 +0900

    Update hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+void compute_hexa(int c, char hexa[16]) {
+    //R compute
+    float div_r = c / 16.;
+    int r_int1 = (int) div_r;
+    printf("%c", hexa[r_int1]);
+
+    float remainder = div_r - r_int1;
+    int r_int2 = (int) (remainder * 16);
+    printf("%c", hexa[r_int2]);
+}
+
+int main() {
+    int r, g, b;
+
+    char hexa[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    scanf("%d", &r);
+    scanf("%d", &g);
+    scanf("%d", &b);
+    
+    printf("The hex code is #");
+    compute_hexa(r, hexa);
+    compute_hexa(g, hexa);
+    compute_hexa(b, hexa);
+}
